eedd46b56782ac295f593c6e37485d2cbc59dd3c
SydEng09 <sydneyengblom@gmail.com>
2022-10-18 14:09:04 -0600

    added email

--- WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
+++ WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
@@ -8,6 +8,8 @@ namespace WestwindWebApp.Pages
     public class SendMailModel : PageModel
     {
         [BindProperty]
+        public string FeedbackMessage { get; set; }
+        [BindProperty]
         public string MailUsername { get; set; }
         [BindProperty]
         public string MailAppPassword { get; set; }
@@ -17,18 +19,9 @@ namespace WestwindWebApp.Pages
         public string EmailSubject { get; set; }
         [BindProperty]
         public string EmailMessage { get; set; }
-        
-
-        
-
-        public void OnGet()
+        public void OnPostSendMail()
         {
-            MailUsername = "sydneyengblom@gmail.com";
-            MailAppPassword = "";
-            EmailToAddress = "sydneyengblom@gmail.com";
-            EmailSubject = "Hi";
-            EmailMessage = "using razor page";
-
+            // FeedbackMessage = "<h2>Send Mail button clicked</h2>";
             var sendMailClient = new SmtpClient();
             sendMailClient.Host = "smtp.gmail.com";
             sendMailClient.Port = 587;
@@ -44,7 +37,49 @@ namespace WestwindWebApp.Pages
             var mailMessage = new MailMessage(sendFromAddress, sendToAddress);
             mailMessage.Subject = EmailSubject;
             mailMessage.Body = EmailMessage;
-            sendMailClient.Send(mailMessage);
+
+            try
+            {
+                sendMailClient.Send(mailMessage);
+                FeedbackMessage = "<div class='alert alert-primary'>Email sent</div";
+            }
+            catch (Exception)
+            {
+                FeedbackMessage = "<div class='alert alert-danger'>Error sending email</div";
+            }
+        }
+        public void OnPostClearForm()
+        {
+            FeedbackMessage="<h2>Clear Form button clicked</h2>";
+        }
+        
+
+        
+
+        public void OnGet()
+        {
+            //MailUsername = "sydneyengblom@gmail.com";
+            //MailAppPassword = "";
+            //EmailToAddress = "sydneyengblom@gmail.com";
+            //EmailSubject = "Hi";
+            //EmailMessage = "using razor page";
+
+            //var sendMailClient = new SmtpClient();
+            //sendMailClient.Host = "smtp.gmail.com";
+            //sendMailClient.Port = 587;
+            //sendMailClient.EnableSsl = true;
+            //var sendMailCredentials = new NetworkCredential();
+            //sendMailCredentials.UserName = MailUsername;
+            //sendMailCredentials.Password = MailAppPassword;
+            //sendMailClient.Credentials = sendMailCredentials;
+
+            //var sendFromAddress = new MailAddress(MailUsername);
+            //var sendToAddress = new MailAddress(EmailToAddress);
+
+            //var mailMessage = new MailMessage(sendFromAddress, sendToAddress);
+            //mailMessage.Subject = EmailSubject;
+            //mailMessage.Body = EmailMessage;
+            //sendMailClient.Send(mailMessage);
 
 
         }
