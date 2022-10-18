7dd8c61d4a527d171a0678549191a0c87db00b31
SydEng09 <sydneyengblom@gmail.com>
2022-10-18 14:31:21 -0600

    more email stuff

--- WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
+++ WestwindSolution/WestwindWebApp/Pages/SendMail.cshtml.cs
@@ -7,6 +7,19 @@ namespace WestwindWebApp.Pages
 {
     public class SendMailModel : PageModel
     {
+        public void OnGet()
+        {
+            var gmailUsername = Configuration["GmailCredentials:Username"];
+            var gmailAppPassword = Configuration["GmailCredentials:Password"];
+            FeedbackMessage = $"Gmail Username = {gmailUsername} <br/>";
+            FeedbackMessage +=$"Gmail App Passwowrd ={gmailAppPassword}";
+
+        }
+        private readonly IConfiguration Configuration;
+        public SendMailModel(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
         [BindProperty]
         public string FeedbackMessage { get; set; }
         [BindProperty]
@@ -27,6 +40,8 @@ namespace WestwindWebApp.Pages
             sendMailClient.Port = 587;
             sendMailClient.EnableSsl = true;
             var sendMailCredentials = new NetworkCredential();
+            MailUsername = Configuration["GmailCredentials:Username"];
+            MailAppPassword= Configuration["GmailCredentials:Password"];
             sendMailCredentials.UserName = MailUsername;
             sendMailCredentials.Password = MailAppPassword;
             sendMailClient.Credentials = sendMailCredentials;
@@ -53,36 +68,6 @@ namespace WestwindWebApp.Pages
             FeedbackMessage="<h2>Clear Form button clicked</h2>";
         }
         
-
-        
-
-        public void OnGet()
-        {
-            //MailUsername = "sydneyengblom@gmail.com";
-            //MailAppPassword = "";
-            //EmailToAddress = "sydneyengblom@gmail.com";
-            //EmailSubject = "Hi";
-            //EmailMessage = "using razor page";
-
-            //var sendMailClient = new SmtpClient();
-            //sendMailClient.Host = "smtp.gmail.com";
-            //sendMailClient.Port = 587;
-            //sendMailClient.EnableSsl = true;
-            //var sendMailCredentials = new NetworkCredential();
-            //sendMailCredentials.UserName = MailUsername;
-            //sendMailCredentials.Password = MailAppPassword;
-            //sendMailClient.Credentials = sendMailCredentials;
-
-            //var sendFromAddress = new MailAddress(MailUsername);
-            //var sendToAddress = new MailAddress(EmailToAddress);
-
-            //var mailMessage = new MailMessage(sendFromAddress, sendToAddress);
-            //mailMessage.Subject = EmailSubject;
-            //mailMessage.Body = EmailMessage;
-            //sendMailClient.Send(mailMessage);
-
-
-        }
         
 
     }
