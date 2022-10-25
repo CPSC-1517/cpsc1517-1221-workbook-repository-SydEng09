71412555cdd2952d1149aba088c2ba6645224092
SydEng09 <sydneyengblom@gmail.com>
2022-10-25 14:48:08 -0600

    added

--- WestwindSolution/WestWindSystem/Entities/BuildVersion.cs
+++ WestwindSolution/WestWindSystem/Entities/BuildVersion.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace WestWindSystem.Entities
 {
     public class BuildVersion
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Major { get; set; }
+        public string Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
--- WestwindSolution/WestWindSystem/Entities/Category.cs
+++ WestwindSolution/WestWindSystem/Entities/Category.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WestWindSystem.Entities
 {
+    [Table(name:"Categories")]
     public class Category
     {
+        [Key]
+        [Column(name:"CategoryID")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Category Name is required")]
+        [MaxLength(15, ErrorMessage ="Max Lenght is 15 characters")]
+        public string CategoryName { get; set; } = String.Empty;
+        [Column(TypeName = "ntext")] //only use column if it isnt the same name as in the database
+        public string? Description { get; set; }
+        [Column(TypeName ="varbinary")]
+        public byte[]? Picture { get; set; }
+        public string? PictureMimeType { get; set; }
 
     }
 }
